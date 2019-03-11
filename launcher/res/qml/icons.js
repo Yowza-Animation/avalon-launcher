ef5bf266e7b8a154218834f5b995bb718b59179d
Jakub Trllo <jakub.trllo@gmail.com>
2019-03-11 18:03:34 +0100

    added clockify icons to launcher

--- launcher/res/qml/icons.js
+++ launcher/res/qml/icons.js
@@ -5,4 +5,6 @@ var map = {
     "maya_icon": "icons/maya.png",
     "houdini_icon": "icons/houdini.png",
     "python_icon": "icons/python.png",
+    "clockify_icon": "icons/clockify.png",
+    "clockify_white_icon": "icons/clockify-white.png"
 }
